1816571413253557171f66f8cb166c071668a758
Nur Islam <mdnurislam15177@gmail.com>
2026-02-25 21:39:51 +0600

    feat: setup mongodb connection using mongoose and dotenv

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./src/config/db');
 
 dotenv.config();
-const app = express();
 
+connectDB();
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
